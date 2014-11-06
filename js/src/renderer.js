e10dc5220a4257fec11bdabde659c3a16e3f10be
Adam Whittingham <adam.whittingham@gmail.com>
2014-11-06 15:05:34 +0000

    Move from drawing nodes as SVGs to divs

--- js/src/renderer.js
+++ js/src/renderer.js
@@ -1,73 +1,30 @@
 (function(){
   window.cabinette.Renderer = function(){
-    var font_size = 12;
-    var spacing = 4;
-    var columns = {};
-    var paper, canvas_width, canvas_height;
+    var display, paper;
 
     var init = function(){
-      paper = Raphael("display", "100%", "100%");
+      display = $('#display');
+      paper = Raphael('links', display.width(), display.height());
       $(cabinette).on('render', render);
     };
 
-    var calculate_canvas_dimensions = function(){
-      canvas_width = paper.canvas.clientWidth;
-      canvas_height = paper.canvas.clientHeight;
-    };
-
-    var calculate_column_coords = function(node_groups){
-      columns.space = canvas_width / Object.keys(node_groups).length;
-      columns.coords = [];
-      var height = canvas_height - spacing;
-      var width = columns.space - spacing;
-      var x = 0;
-      _.each(node_groups, function(node_group, header){
-        centre = x + (width/2);
-        columns.coords.push(
-          {
-            x: x,
-            centre: centre,
-            height: height,
-            width: width,
-            heading: { x: centre, y: spacing + font_size},
-            node: {
-              x: x + spacing/2,
-              centre: centre,
-              width: width - spacing,
-              height: width /2
-            }
-          }
-        );
-        x += columns.space;
-      });
-    };
-
-    var draw_column_contents = function(node_group, column){
-      var y = 2 * font_size + spacing;
-      _.each(node_group, function(node){
-        paper.rect(column.node.x, y, column.node.width, column.node.height, 5).node.setAttribute("class","node");
-        paper.text(column.centre, y+ font_size, node.name);
-        y += column.node.height + spacing;
-      });
-    };
-
-    var draw_columns = function(node_groups){
-      var y = spacing / 2;
-      var c_index = 0;
-
-      _.each(node_groups, function(node_group, header){
-        var col = columns.coords[c_index];
-        paper.rect(col.x, y, col.width, col.height, 5).node.setAttribute("class","column");
-        paper.text(col.heading.x, col.heading.y, header);
-        draw_column_contents(node_group, col);
-        c_index++;
+    var create_columns = function(columns){
+      _.each(columns, function(column, heading){
+        var col_id = heading.toLowerCase().trim().replace(/ /g, '_');
+        column.div = $('<div class="column '+col_id+'"></div>');
+        column.div.append('<h3>'+heading+'</h3>');
+        _.each(column, function(cell){
+          cell.div = $('<div class="cell">' + cell.name +'</div>');
+          column.div.append(cell.div);
+        });
+        $(display).append(column.div);
       });
     };
 
     var render = function(event, options){
-      calculate_canvas_dimensions();
-      calculate_column_coords(options.data);
-      draw_columns(options.data);
+      create_columns(options.data);
+      paper.path('M0,0 L250,250');
+
       options.complete(options.data);
     };
 
