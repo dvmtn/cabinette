7f00dd42e1443dcfa5d34430f829e9c15e361eb3
Adam Whittingham <adam.whittingham@gmail.com>
2014-11-07 14:14:35 +0000

    Split setting and unsetting active into seperate functions

--- js/src/highlighter.js
+++ js/src/highlighter.js
@@ -1,16 +1,25 @@
 (function(){
   window.cabinette.Highlighter = function(){
-    var highlight = function(event, element_id){
+
+    var unset_active = function(){
       $('.cell.active').removeClass('active');
       $('path').attr('class', function(index, classNames) {
         return classNames.replace('active', '');
       });
+    };
 
+    var set_active = function(element_id){
       $('.cell.' + element_id).addClass('active');
       $('path.' + element_id).attr('class', function(index, classNames) {
         return classNames + ' active';
       });
     };
+
+    var highlight = function(event, element_id){
+      unset_active();
+      set_active(element_id);
+    };
+
     $(cabinette).on('highlight', highlight);
   };
 }());
