eeff2139af4b32911472962e6edc3a26be2093b5
Adam Whittingham <adam.whittingham@gmail.com>
2014-11-10 15:58:25 +0000

    Fix reflow issue due to changing border size

--- specs/ui_spec.js
+++ specs/ui_spec.js
@@ -5,7 +5,7 @@ describe("UI", function() {
 
   it("generates option strings", function(){
     var input = ['foo', 'bar', 'baz'];
-    var expected = '<option value="bar">bar</option>\n<option value="bar">bar</option>\n<option value="foo">foo</option>';
+    var expected = '<option value="bar">bar</option>\n<option value="baz">baz</option>\n<option value="foo">foo</option>';
     $(cabinette).trigger('populate_finder', {
       values: input,
       complete: function(actual){
